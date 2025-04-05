0db1af2cbdfddefe3dc252d906bf73ba9ff00370
aabb11122 <pakotang9@gmail.com>
2025-04-05 16:50:53 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
       <p>Digital portal for managing your strata building.</p>
 
       <h2>About Our Building</h2>
-      <img src="/building.jpg" alt="Our Building" width="300" />
+      <img src="/buliding.jpg" alt="Our Building" width="300" />
 
       {/* Display the environment variable */}
       <p>Contact Email: {process.env.NEXT_PUBLIC_CONTACT_EMAIL}</p>
